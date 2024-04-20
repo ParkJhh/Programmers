52e431539edb76c6b062967bf88a5bab0de416a7
ParkJhh <qkrwnsgur145@gmail.com>
2024-04-20 12:38:26 +0900

    Lv1 가장 가까운 글자 java로 문제 풀이

--- src/java/Lv1/정답률80_60/가장_가까운_글자.java
+++ src/java/Lv1/정답률80_60/가장_가까운_글자.java
@@ -1,11 +1,8 @@
-import java.util.*;
-import java.util.Map;
-import java.util.HashSet;
-import java.util.Set;
+package java.Lv1.정답률80_60;
 
-public class newAnswer{
-    public static void main(String[] args) {
-        String s = "banana";
+import java.util.*;
+public class 가장_가까운_글자 {
+    public ArrayList<Integer> solution(String s) {
         ArrayList<Character> chk = new ArrayList<>();
         ArrayList<Integer> answer = new ArrayList<>();
 
@@ -17,6 +14,6 @@ public class newAnswer{
             }
             chk.add(s.charAt(i));
         }
-        System.out.println(answer);
+        return answer;
     }
 }
