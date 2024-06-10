86875b9c9eedebabd227da56fffcd3e27bf738ed
ParkJhh <qkrwnsgur145@gmail.com>
2024-06-10 14:36:31 +0900

    Lv2 피보나치 수  java로 문제 풀이

--- src/java/Lv2/정답률80_60/피보나치_수.java
+++ src/java/Lv2/정답률80_60/피보나치_수.java
@@ -1,19 +1,13 @@
-import java.lang.reflect.Array;
-import java.util.*;
-import java.util.Map;
-import java.util.HashSet;
-import java.util.Set;
+package java.Lv2.정답률80_60;
 
-public class newAnswer{
-    public static void main(String[] args) {
-        int n = 3;
-
+public class 피보나치_수 {
+    public int solution(int n) {
         int answer = 0;
         int num1 = 1;
         int num2 = 1;
 
         if(n == 1 || n == 2){
-            answer = 1;
+            return 1;
         } else {
             for(int i = 3; i <= n; i++){
                 answer = (num1+num2) % 1234567;
@@ -22,8 +16,7 @@ public class newAnswer{
                 //F(1)
                 num2 = answer;
             }
+            return answer;
         }
-        System.out.println(answer);
     }
 }
-
